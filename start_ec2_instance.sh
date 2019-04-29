a963712c8f33bc6376b607bf4ee3e8d7764aa061
Anish Raghavan <raghavaa@tcd.ie>
2019-04-29 10:55:28 +0100

    test

--- start_ec2_instance.sh
+++ start_ec2_instance.sh
@@ -1,4 +1,4 @@
-instance_id=$(aws ec2 run-instances --region us-west-2 --image-id ami-d732f0b7 --count 1 --instance-type t2.micro --key-name k1 --query 'Instances[0].InstanceId')
+dsfinstance_id=$(aws ec2 run-instances --region us-west-2 --image-id ami-d732f0b7 --count 1 --instance-type t2.micro --key-name k1 --query 'Instances[0].InstanceId')
 echo instance_id:$instance_id
 aws ec2 wait instance-running --region us-west-2 --instance-ids $instance_id
 ip_address=$(aws ec2 describe-instances --region us-west-2 --instance-ids $instance_id --query 'Reservations[0].Instances[0].PublicIpAddress')
